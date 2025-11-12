97cdd7d65ab495d0666d66b272483fe24802bb51
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2025-11-11 22:02:34 -0500

    added contact links

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,17 +30,27 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className= "border-b border-white/10 px-30 py-6">
-          <nav className="flex space-x-8">
-            <Link href="/" className = "test-grey-800 font-medium">
-            About
-            </Link>
-            <Link href="/" className = "test-grey-800 font-medium">
-            Projects
-            </Link>
-            <Link href="/" className = "test-grey-800 font-medium">
-            Experience
-            </Link>
+        <header className= "border-b border-white/10 px-17 py-6">
+          <nav className="flex justify-between items-center">
+            <div className="flex items-center space-x-10">
+              <Link href="/" className = "text-gray-200 hover:text-white font-medium hover:underline">
+              About
+              </Link>
+              <Link href="/" className = "text-gray-200 hover:text-white font-medium hover:underline">
+              Projects
+              </Link>
+              <Link href="/" className = "text-gray-200 hover:text-white font-medium hover:underline">
+              Experience
+              </Link>
+            </div>
+
+            <div className="flex items-center space-x-10">
+              
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/ethan-rogers/portfolio-website" className = "flex items-center space-x-2 text-gray-400 font-medium hover:underline">
+              <FaGithub /> <span>ethan-rogers/portfolio-website</span>
+              </Link>
+
+            </div>
 
           </nav>
         </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
 import Image from "next/image";
+import Link from 'next/link';
+import { FaLinkedin } from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
+import { FaGithub } from "react-icons/fa";
+
 
 export default function Home() {
-  return <div className ="text-left pt-12 px-30">
-    <h1 className="text-6xl mb-6"> Welcome :)</h1>
+  return <div className ="text-left pt-12 px-17 text-gray-200 ">
+    <h1 className="text-7xl mb-15 mt-10"> Welcome :)</h1>
+    <br></br>
+    <p className="text-2xl mb-6"> My name is Ethan Rogers. I am studying computer engineering at the University of Virginia. 
+      In my free time, I love coding software, designing cool electronics and fencing. 
+      Check out the projects I've worked on. 
+      If you have any questions or want to contribute to these projects, feel free to email me.</p>
 
-    <p className="text-2xl mb-6"> My name is Ethan Rogers, thanks for visiting my website! I am studying computer engineering at the University of Virginia. 
-      In my free time, I love building software, designing cool electronics and fencing.</p>
+         <div className="flex items-center space-x-10 mt-25">
+              <Link target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ethan-rogers-188394329" className = "text-4xl text-gray-200 hover:text-white font-medium">
+                <FaLinkedin /> 
+              </Link>
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/ethan-rogers" className = "text-4xl text-gray-200 hover:text-white font-medium">
+                <FaGithub />
+              </Link>
+              <Link target="_blank" rel="noopener noreferrer" href="mailto:eholtrogers@gmail.com" className = "text-4xl text-gray-200 hover:text-white font-medium">
+                <CgMail /> 
+              </Link>
 
+              
+            </div>
   </div>;
+
+
 }
